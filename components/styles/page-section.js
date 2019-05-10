9859bc2a815bc0ca108d717ce46e823977ae8d76
Aaron Young <mustarddevelopment@gmail.com>
2019-05-09 23:21:41 -0700

    added logo for call to action

---
+++ components/styles/page-section.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const PageSection = styled.section`
+  display: grid;
+  grid-gap: 1rem;
+
+  @media only screen and (min-width: 1224px) {
+    margin: 1rem 3rem 3rem;
+  }
+`;
+
+export default PageSection;
---
+++ components/styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    midnightBlack: "#011717",
+    darkChicken: "#BA8B49",
+    lightChicken: "#EDDB8F",
+    ghostWhite: "#FAF8FD"
+  },
+  fonts: {
+    default: "1rem"
+  }
+};
+
+export default theme;
